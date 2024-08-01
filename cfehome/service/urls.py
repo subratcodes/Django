afcd38a5a881a2633cf8417d8376db88a434b502
Subrat Kumar Singh <subratsinghsks_cse16@its.edu.in>
2024-07-31 22:49:41 -0400

    view sets done

--- cfehome/service/urls.py
+++ cfehome/service/urls.py
@@ -1,16 +1,25 @@
-from django.urls import path
+from django.urls import path,include
 from .views import *
 from .viewV2 import ProductView
 from .mixins import ProductList,SingleProduct
 from .generics import ProductList
+from rest_framework.routers import DefaultRouter
+from .viewsets import ProductViewSets
+
+router=DefaultRouter()
+router.register('students',ProductViewSets)
 
 urlpatterns=[
-     path('products/all',ProductList.as_view()),
-     path('products/<int:pk>',SingleProduct.as_view()),
-   #  path("products/create",MyOwnView.as_view()),
-#     path("products/retrive/<pk>",RetrieveProductView.as_view()),
-#     path("products/update/<pk>",UpdateAPIView.as_view()),
-#     path("products/delete/<pk>",DestoryProductView.as_view()),
-   # path("products/custom",MyOwnView.as_view())
-   path("products/v2",ProductView.as_view())
+       path('',include(router.urls)),
+#      path('products/all',ProductList.as_view()),
+#      path('products/<int:pk>',SingleProduct.as_view()),
+#    #  path("products/create",MyOwnView.as_view()),
+# #     path("products/retrive/<pk>",RetrieveProductView.as_view()),
+# #     path("products/update/<pk>",UpdateAPIView.as_view()),
+# #     path("products/delete/<pk>",DestoryProductView.as_view()),
+#    # path("products/custom",MyOwnView.as_view())
+#    path("products/v2",ProductView.as_view())
  ]  
+
+
+
